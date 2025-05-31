54d00f0c5f432df8f6da2e4b71bc2476c07018b4
lodiouf <louisisaacdf@gmail.com>
2025-05-31 03:30:10 +0000

    Update

--- orientation_tester/lib/main.dart
+++ orientation_tester/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_rotation_sensor/flutter_rotation_sensor.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +12,148 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  double _ax = 0, _ay = 0, _az = 0;
+  double _mx = 0, _my = 0, _mz = 0;
+  double _azimuth = 0, _pitch = 0, _roll = 0;
+  // Ajout des variables pour les angles calculés
+  double _calcAzimuth = 0, _calcPitch = 0, _calcRoll = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  StreamSubscription? _accelerometerStream;
+  StreamSubscription? _magnetometerStream;
+  StreamSubscription? _orientationStream;
+
+  Text title(String txt) {
+    return Text(
+      txt,
+      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    );
+  }
+
+  // Fonction pour calculer les angles d'Euler
+  void _calculateEulerAngles() {
+    // 1. Calcul du roulis et du tangage à partir de l'accéléromètre
+    _calcRoll = atan2(_ay, _az) * 180 / pi;
+    _calcPitch = atan2(-_ax, sqrt(_ay * _ay + _az * _az)) * 180 / pi;
+
+    // 2. Calcul du lacet (azimut) avec compensation magnétique
+    final double rollRad = _calcRoll * pi / 180;
+    final double pitchRad = _calcPitch * pi / 180;
+
+    final double cosRoll = cos(rollRad);
+    final double sinRoll = sin(rollRad);
+    final double cosPitch = cos(pitchRad);
+    final double sinPitch = sin(pitchRad);
+
+    // Composantes magnétiques compensées
+    final double magX = _mx * cosPitch + _mz * sinPitch;
+    final double magY =
+        _mx * sinRoll * sinPitch + _my * cosRoll - _mz * sinRoll * cosPitch;
+
+    // 3. Calcul de l'azimut
+    _calcAzimuth = atan2(-magY, magX) * 180 / pi;
+
+    // Normaliser entre 0-360°
+    if (_calcAzimuth < 0) _calcAzimuth += 360;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _magnetometerStream = magnetometerEvents.listen((MagnetometerEvent event) {
+      setState(() {
+        _accelerometerStream = accelerometerEvents.listen((
+          AccelerometerEvent event,
+        ) {
+          _ax = event.x;
+          _ay = event.y;
+          _az = event.z;
+        });
+        
+        _mx = event.x;
+        _my = event.y;
+        _mz = event.z;
+        _calculateEulerAngles();
+      });
+    });
+
+    _orientationStream = RotationSensor.orientationStream.listen((event) {
+      setState(() {
+        _azimuth = event.eulerAngles.azimuth;
+        _pitch = event.eulerAngles.pitch;
+        _roll = event.eulerAngles.roll;
+      });
     });
   }
 
   @override
+  void dispose() {
+    _accelerometerStream?.cancel();
+    _magnetometerStream?.cancel();
+    _orientationStream?.cancel();
+    super.dispose();
+  }
+
+  String format(double value) {
+    return value.toStringAsFixed(2);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.blue,
+        title: const Text('Sensors Checker'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            title('Accelerometer'),
+            Text('${format(_ax)}, ${format(_ay)}, ${format(_az)}'),
+
+            const SizedBox(height: 20),
+            title('Magnetometer'),
+            Text('${format(_mx)}, ${format(_my)}, ${format(_mz)}'),
+
+            const SizedBox(height: 20),
+            title('Orientation (Sensor)'),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Azimuth: ${format(_azimuth)}\n'
+              'Pitch: ${format(_pitch)}\n'
+              'Roll: ${format(_roll)}',
+            ),
+
+            // Ajout de la section pour les angles calculés
+            const SizedBox(height: 30),
+            title('Calculated Orientation'),
+            Text(
+              'Azimuth: ${format(_calcAzimuth)}\n'
+              'Pitch: ${format(_calcPitch)}\n'
+              'Roll: ${format(_calcRoll)}',
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
